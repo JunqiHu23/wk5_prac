7b194e41a39ac5916ddd56885a7c58450120015e
JunqiHu23 <junqi.hu.21@ucl.ac.uk>
2021-11-04 00:40:35 +0000

    static map

--- week5_prac.R
+++ week5_prac.R
@@ -73,3 +73,62 @@ WorldCities2 <- WorldCities %>%
            WorldCities$CITY_NAME=='Edinburgh')
 
 newbb <- c(xmin=-296000,ymin=5408,xmax=655696,ymax=1000000)
+
+UK_outlinecrop <- UK_outline$geometry %>%
+  st_crop(.,newbb)
+
+Hotels <- Hotels %>%
+  #at the moment each hotel is a row for the borough
+  #we just one one row that has number of airbnbs
+  group_by(.,GSS_CODE,NAME) %>%
+  summarise('Accomadation count'=unique(hotels_in_borough))
+
+Airbnb <- Airbnb %>%
+  summarise('Accomadation count'=unique(hotels_in_borough))
+
+
+
+#make the map
+
+tmap_mode('plot')
+
+#set the breaks
+#for our mapped data
+
+breaks=c(0,5,12,26,57,286)
+
+#plot each map
+tm1 <- tm_shape(Hotels) +
+  tm_polygons('Accomadation count',
+              breaks=breaks,
+              palette='PuBu') +
+  tm_legend(show = FALSE) +
+  tm_layout(frame = FALSE) +
+  tm_credits('(a)',position = c(0,0.85),size=1.5)
+
+tm2 <- tm_shape(Airbnb) +
+  tm_polygons('Accomadation count',
+              breaks=breaks,
+              palette='PuBu') +
+  tm_legend(show = FALSE) +
+  tm_layout(frame = FALSE) +
+  tm_credits('(b)',position = c(0,0.85),size=1.5)
+
+tm3 <- tm_shape(UK_outlinecrop) +
+  tm_polygons(col='darkslategray1') +
+  tm_layout(frame = FALSE) +
+  tm_shape(WorldCities2) +
+  tm_symbols(col='red',scale = .5)+
+  tm_text('CITY_NAME',xmod=-1,ymod=-0.5)
+
+legend <- tm_shape(Hotels) +
+  tm_polygons('Accomodation count',
+              palette='PuBu') +
+  tm_scale_bar(position = c(0.2,0.04),text.size = 0.6) + 
+  tm_compass(north = 0,position = c(0.65,0.6)) +
+  tm_layout(legend.only = TRUE, legend.position = c(0.2,0.25), asp = 0.1) +
+  tm_credits('(c)OpenStreetMap contributors and Airbnb', position = c(0.0,0.0))
+
+t=tmap_arrange(tm1,tm2,tm3,legend,ncol = 2)
+
+t
